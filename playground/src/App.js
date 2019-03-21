81cbf3c6dc30d2e95e9b0d2748126e40908d6667
Nathan Krasney <natankrasney@gmail.com>
2019-03-21 14:07:59 +0200

    fetch posts

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import TwoCountersOption2 from './TwoCountersOption2'
+import Posts from './Posts'
 
 class App extends Component {
   render() {
     return <div style={{margin:'10px'}}>
-      <TwoCountersOption2/>
+      <Posts/>
     </div>;
   }
 }
