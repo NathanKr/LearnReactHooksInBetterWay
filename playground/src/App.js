1b89844a8b4d1f105236b79315e37d1178dd3070
Nathan Krasney <natankrasney@gmail.com>
2019-03-24 10:28:42 +0200

    show clicks using class\hooks

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import Posts from './Posts'
+import ShowClickUsingHooks from './ShowClickUsingHooks'
+
 
 class App extends Component {
   render() {
     return <div style={{margin:'10px'}}>
-      <Posts/>
+      <ShowClickUsingHooks/>
     </div>;
   }
 }
