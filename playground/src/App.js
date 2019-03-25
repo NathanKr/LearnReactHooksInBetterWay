d567afb868c2e4682fd46c739f0ca30d7a7665dc
Nathan Krasney <natankrasney@gmail.com>
2019-03-25 15:51:37 +0200

    setInterval with useRef

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,21 +1,10 @@
-import React, { useState } from "react";
-import { Flag , Segment } from "semantic-ui-react";
-import Level1 from "./Level1";
-import { LanguageContext } from "./LanguageContext";
+import React from "react";
+import SetIntervalSampleGood from "./SetIntervalSampleGood";
 
 const App = () => {
-  const [language, setLanguage] = useState("english");
-
-  const langObj = { language };
   return (
     <div style={{ margin: "10px" }}>
-    <Segment>
-      <Flag onClick={() => setLanguage("english")} name="us" />
-      <Flag onClick={() => setLanguage("portuguese")} name="br" />
-      </Segment>
-      <LanguageContext.Provider value={langObj}>
-        <Level1 />
-      </LanguageContext.Provider>
+      <SetIntervalSampleGood />
     </div>
   );
 };
