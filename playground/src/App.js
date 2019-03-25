f721fd500aa9f525475f804d420c6c50f259f263
Nathan Krasney <natankrasney@gmail.com>
2019-03-25 14:32:36 +0200

    language context with useContext

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
-import Clock from "./Clock";
+import { Flag , Segment } from "semantic-ui-react";
+import Level1 from "./Level1";
+import { LanguageContext } from "./LanguageContext";
 
 const App = () => {
-  const [showClock, setShowClock] = useState(true);
+  const [language, setLanguage] = useState("english");
 
+  const langObj = { language };
   return (
-    <div onClick={() => setShowClock(!showClock)} style={{ margin: "10px" }}>
-      {showClock ? <Clock /> : ''}
+    <div style={{ margin: "10px" }}>
+    <Segment>
+      <Flag onClick={() => setLanguage("english")} name="us" />
+      <Flag onClick={() => setLanguage("portuguese")} name="br" />
+      </Segment>
+      <LanguageContext.Provider value={langObj}>
+        <Level1 />
+      </LanguageContext.Provider>
     </div>
   );
 };
