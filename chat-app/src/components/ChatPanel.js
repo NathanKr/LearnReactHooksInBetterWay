7f4bc759fb80b40eeabb875c796468c82848f8aa
Nathan Krasney <natankrasney@gmail.com>
2019-04-01 17:20:40 +0300

    retrive messages with a problem

--- chat-app/src/components/ChatPanel.js
+++ chat-app/src/components/ChatPanel.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ChatPanel.css";
 import firebase from "../logic/firebase";
 
 const ChatPanel = () => {
+  const [messages, setMessages] = useState([]);
+
   const messagesRefFirebase = firebase.database().ref("messages");
+
+  const addMessagesListener = () => {
+    console.log("addMessagesListener");
+    messagesRefFirebase.on("child_added", snap => {
+      console.log(snap.val());
+      const newMessages = [...messages];
+      newMessages.push(snap.val());
+      setMessages(newMessages);
+    });
+  };
+
+  const removeMessagesListener = () => {
+    console.log("removeMessagesListener");
+    messagesRefFirebase.off();
+  };
+
+  useEffect(() => {
+    addMessagesListener();
+
+    return () => removeMessagesListener();
+  }, []);
+
+  const messagesElements = (
+    <ul>
+      {messages.map((item, index) => (
+        <li key={index}>{item.text}</li>
+      ))}
+    </ul>
+  );
 
   return (
     <div className="ChatPanel">
       <div>room1</div>
-      <div style={{ flexGrow: 1 }}>
-        <ul>
-          <li>message 1</li>
-          <li>message 2</li>
-          <li>message 3</li>
-          <li>message 4</li>
-        </ul>
-      </div>
+      <div style={{ flexGrow: 1 }}>{messagesElements}</div>
       <div style={{ display: "flex" }}>
         <input style={{ flexGrow: 1 }} placeholder="insert message ..." />
         <button
