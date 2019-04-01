64f4555ac0297c511aed0af613bca22b5a12c022
Nathan Krasney <natankrasney@gmail.com>
2019-04-01 16:07:48 +0300

    send message to firebase database

--- chat-app/src/components/ChatPanel.js
+++ chat-app/src/components/ChatPanel.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import './ChatPanel.css'
+import React from "react";
+import "./ChatPanel.css";
+import firebase from "../logic/firebase";
 
 const ChatPanel = () => {
-    return (
-        <div className='ChatPanel'>
-            <div>room1</div>
-            <div style={{flexGrow:1}}>
-            <ul>
-                <li>message 1</li>
-                <li>message 2</li>
-                <li>message 3</li>
-                <li>message 4</li>
-            </ul>
-            </div>
-            <div style={{display:'flex'}}>
-                <input style={{flexGrow:1}} placeholder="insert message ..."/>
-                <button>SendMessage</button>
-            </div>
-        </div>
-    );
+  const messagesRefFirebase = firebase.database().ref("messages");
+
+  return (
+    <div className="ChatPanel">
+      <div>room1</div>
+      <div style={{ flexGrow: 1 }}>
+        <ul>
+          <li>message 1</li>
+          <li>message 2</li>
+          <li>message 3</li>
+          <li>message 4</li>
+        </ul>
+      </div>
+      <div style={{ display: "flex" }}>
+        <input style={{ flexGrow: 1 }} placeholder="insert message ..." />
+        <button
+          onClick={() =>
+            messagesRefFirebase
+              .child("id2")
+              .set({ text: "some text 2" })
+              .then(msg => console.log(`set success : ${msg}`))
+              .catch(err => console.log(`set error : ${err} `))
+          }
+        >
+          SendMessage
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ChatPanel;
