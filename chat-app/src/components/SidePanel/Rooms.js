293884718da9355c8cdd09c7fd614add8d33977e
Nathan Krasney <natankrasney@gmail.com>
2019-04-02 17:41:26 +0300

    setCurrentRoom

--- chat-app/src/components/SidePanel/Rooms.js
+++ chat-app/src/components/SidePanel/Rooms.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import CurrentRoomContext from "../../context/CurrentRoomContext";
 
 const Rooms = () => {
+  const { setCurrentRoom } = useContext(CurrentRoomContext);
   return (
     <div>
       <ul>
-        <li>room1</li>
-        <li>room2</li>
+        <li
+          onClick={() =>
+            setCurrentRoom({
+              id: "idroom1",
+              name: "room1",
+              description: "desc1"
+            })
+          }
+        >
+          room1
+        </li>
+        <li
+          onClick={() =>
+            setCurrentRoom({
+              id: "idroom2",
+              name: "room2",
+              description: "desc2"
+            })
+          }
+        >
+          room2
+        </li>
       </ul>
     </div>
   );
