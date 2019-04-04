9e68bee5c33dc1e9b2d941c2fa895d1092d40ab1
Nathan Krasney <natankrasney@gmail.com>
2019-04-04 11:29:08 +0300

    add room to firebase database

--- chat-app/src/components/SidePanel/Rooms.js
+++ chat-app/src/components/SidePanel/Rooms.js
@@ -1,12 +1,34 @@
 import React, { useContext } from "react";
 import CurrentRoomContext from "../../context/CurrentRoomContext";
+import firebase from "../../logic/firebase";
 
 const Rooms = () => {
   const { setCurrentRoom } = useContext(CurrentRoomContext);
+  const roomsRefFirebase = firebase.database().ref("rooms");
+
+  const addRoom = () => {
+    console.log("addRoom");
+    const roomId = roomsRefFirebase.push().key;
+
+    const newRoom = {
+      id: roomId,
+      name: "room2", //todo replace with data from modal
+      description: "desc2" //todo replace with data from modal
+    };
+
+    roomsRefFirebase
+      .child(roomId)
+      .set(newRoom)
+      .then(room => console.log(`success : ${room}`))
+      .catch(err => console.log(`error : ${err}`));
+  };
+
   return (
     <div>
+      <p>rooms (...)</p>
+      <button onClick={addRoom}>Add Room</button>
       <ul>
-        <li
+        {/* <li
           onClick={() =>
             setCurrentRoom({
               id: "idroom1",
@@ -27,7 +49,7 @@ const Rooms = () => {
           }
         >
           room2
-        </li>
+        </li> */}
       </ul>
     </div>
   );
