c472c76c43caee00d2b32a87f6af94ce10bffb51
Nathan Krasney <natankrasney@gmail.com>
2019-04-08 13:46:41 +0300

    handle onAuthStateChanged

--- chat-app/src/components/Root.js
+++ chat-app/src/components/Root.js
@@ -1,12 +1,28 @@
-import React , {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import ChatApp from "./ChatApp";
 import Login from "../components/Auth/Login";
 import Register from "../components/Auth/Register";
 import UserContext from "../context/UserContext";
+import firebase from "../logic/firebase";
 
-const Root = () => {
-  const [user, setUser] = useState(null);
+const Root = ({ history }) => {
+  const [user, setUser] = useState({ displayName: "", uid: "", photoURL: "" });
+
+  useEffect(() => {
+    console.log("mount");
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log("allready logged in -> navigate to /");
+        const { displayName, uid, photoURL } = user;
+        setUser({ displayName, uid, photoURL });
+        history.push("/");
+      } else {
+        console.log("not logged in -> navigate to /Login");
+        history.push("/Login");
+      }
+    });
+  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
